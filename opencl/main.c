7051ceaaf49583ff1b1d80607c099eacb53b47a7
Rory Clear <roryclear.rc@gmail.com>
2023-03-17 10:37:18 +0000

    start matmul

--- opencl/main.c
+++ opencl/main.c
@@ -18,6 +18,18 @@ const char *KernelSource = "\n" \
 "       output[i] = input[i] * input[i];                                \n" \
 "}                                                                      \n" \
 "\n";
+
+const char *KernelSource2 = "\n" \
+"__kernel void matmul(                                                       \n" \
+"   __global float* a,                                              \n" \
+"   __global float* output,                                             \n" \
+"   const unsigned int count)                                           \n" \
+"{                                                                      \n" \
+"   int i = get_global_id(0);                                           \n" \
+"   if(i < count)                                                       \n" \
+"       output[i] = a[i] * a[i];                                \n" \
+"}                                                                      \n" \
+"\n";
  
 ////////////////////////////////////////////////////////////////////////////////
  
@@ -29,11 +41,11 @@ int main(void)
     clGetDeviceIDs(NULL, CL_DEVICE_TYPE_GPU, 1, &device_id, NULL);
     cl_context context = clCreateContext(0, 1, &device_id, NULL, NULL, &err);
     cl_command_queue commands = clCreateCommandQueue(context, device_id, 0, &err);
-    cl_program program = clCreateProgramWithSource(context, 1, (const char **) & KernelSource, NULL, &err);
+    cl_program program = clCreateProgramWithSource(context, 1, (const char **) & KernelSource2, NULL, &err);
     clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
-    cl_kernel kernel = clCreateKernel(program, "square", &err);
+    cl_kernel kernel = clCreateKernel(program, "matmul", &err);
     
-    cl_mem input = clCreateBuffer(context,  CL_MEM_READ_ONLY,  sizeof(float) * DATA_SIZE, NULL, NULL);
+    cl_mem inputA = clCreateBuffer(context,  CL_MEM_READ_ONLY,  sizeof(float) * DATA_SIZE, NULL, NULL);
     cl_mem output = clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(float) * DATA_SIZE, NULL, NULL);
     
     float data[DATA_SIZE];
@@ -42,8 +54,8 @@ int main(void)
         data[i] = i;
     }
     
-    err = clEnqueueWriteBuffer(commands, input, CL_TRUE, 0, sizeof(float) * DATA_SIZE, data, 0, NULL, NULL);
-    clSetKernelArg(kernel, 0, sizeof(cl_mem), &input);
+    err = clEnqueueWriteBuffer(commands, inputA, CL_TRUE, 0, sizeof(float) * DATA_SIZE, data, 0, NULL, NULL);
+    clSetKernelArg(kernel, 0, sizeof(cl_mem), &inputA);
     clSetKernelArg(kernel, 1, sizeof(cl_mem), &output);
     unsigned int count = DATA_SIZE;
     clSetKernelArg(kernel, 2, sizeof(unsigned int), &count);
@@ -62,161 +74,11 @@ int main(void)
         if (results[i] == data[i] * data[i]) { correct++; }
     }
     printf("Computed '%d/%d' correct values!\n", correct, count);
-    clReleaseMemObject(input);
+    clReleaseMemObject(inputA);
     clReleaseMemObject(output);
     clReleaseProgram(program);
     clReleaseKernel(kernel);
     clReleaseCommandQueue(commands);
     clReleaseContext(context);
     return 0;
-    
-    // Connect to a compute device
-    //
-    /*
-    int gpu = 1;
-    err = clGetDeviceIDs(NULL, gpu ? CL_DEVICE_TYPE_GPU : CL_DEVICE_TYPE_CPU, 1, &device_id, NULL);
-    if (err != CL_SUCCESS)
-    {
-        printf("Error: Failed to create a device group!\n");
-        return EXIT_FAILURE;
-    }
-  
-    // Create a compute context
-    //
-    context = clCreateContext(0, 1, &device_id, NULL, NULL, &err);
-    if (!context)
-    {
-        printf("Error: Failed to create a compute context!\n");
-        return EXIT_FAILURE;
-    }
- 
-    // Create a command commands
-    //
-    commands = clCreateCommandQueue(context, device_id, 0, &err);
-    if (!commands)
-    {
-        printf("Error: Failed to create a command commands!\n");
-        return EXIT_FAILURE;
-    }
- 
-    // Create the compute program from the source buffer
-    //
-    program = clCreateProgramWithSource(context, 1, (const char **) & KernelSource, NULL, &err);
-    if (!program)
-    {
-        printf("Error: Failed to create compute program!\n");
-        return EXIT_FAILURE;
-    }
- 
-    // Build the program executable
-    //
-    err = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
-    if (err != CL_SUCCESS)
-    {
-        size_t len;
-        char buffer[2048];
- 
-        printf("Error: Failed to build program executable!\n");
-        clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG, sizeof(buffer), buffer, &len);
-        printf("%s\n", buffer);
-        exit(1);
-    }
- 
-    // Create the compute kernel in the program we wish to run
-    //
-    kernel = clCreateKernel(program, "square", &err);
-    if (!kernel || err != CL_SUCCESS)
-    {
-        printf("Error: Failed to create compute kernel!\n");
-        exit(1);
-    }
- 
-    // Create the input and output arrays in device memory for our calculation
-    //
-    input = clCreateBuffer(context,  CL_MEM_READ_ONLY,  sizeof(float) * count, NULL, NULL);
-    output = clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(float) * count, NULL, NULL);
-    if (!input || !output)
-    {
-        printf("Error: Failed to allocate device memory!\n");
-        exit(1);
-    }
-    
-    // Write our data set into the input array in device memory
-    //
-    err = clEnqueueWriteBuffer(commands, input, CL_TRUE, 0, sizeof(float) * count, data, 0, NULL, NULL);
-    if (err != CL_SUCCESS)
-    {
-        printf("Error: Failed to write to source array!\n");
-        exit(1);
-    }
- 
-    // Set the arguments to our compute kernel
-    //
-    err = 0;
-    err  = clSetKernelArg(kernel, 0, sizeof(cl_mem), &input);
-    err |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &output);
-    err |= clSetKernelArg(kernel, 2, sizeof(unsigned int), &count);
-    if (err != CL_SUCCESS)
-    {
-        printf("Error: Failed to set kernel arguments! %d\n", err);
-        exit(1);
-    }
- 
-    // Get the maximum work group size for executing the kernel on the device
-    //
-    err = clGetKernelWorkGroupInfo(kernel, device_id, CL_KERNEL_WORK_GROUP_SIZE, sizeof(local), &local, NULL);
-    if (err != CL_SUCCESS)
-    {
-        printf("Error: Failed to retrieve kernel work group info! %d\n", err);
-        exit(1);
-    }
- 
-    // Execute the kernel over the entire range of our 1d input data set
-    // using the maximum number of work group items for this device
-    //
-    global = count;
-    err = clEnqueueNDRangeKernel(commands, kernel, 1, NULL, &global, &local, 0, NULL, NULL);
-    if (err)
-    {
-        printf("Error: Failed to execute kernel!\n");
-        return EXIT_FAILURE;
-    }
- 
-    // Wait for the command commands to get serviced before reading back results
-    //
-    clFinish(commands);
- 
-    // Read back the results from the device to verify the output
-    //
-    err = clEnqueueReadBuffer( commands, output, CL_TRUE, 0, sizeof(float) * count, results, 0, NULL, NULL );
-    if (err != CL_SUCCESS)
-    {
-        printf("Error: Failed to read output array! %d\n", err);
-        exit(1);
-    }
-    
-    // Validate our results
-    //
-    correct = 0;
-    for(i = 0; i < count; i++)
-    {
-        if(results[i] == data[i] * data[i])
-            correct++;
-    }
-    
-    // Print a brief summary detailing the results
-    //
-    printf("Computed '%d/%d' correct values!\n", correct, count);
-    
-    // Shutdown and cleanup
-    //
-    clReleaseMemObject(input);
-    clReleaseMemObject(output);
-    clReleaseProgram(program);
-    clReleaseKernel(kernel);
-    clReleaseCommandQueue(commands);
-    clReleaseContext(context);
- 
-    return 0;
-     */
 }
