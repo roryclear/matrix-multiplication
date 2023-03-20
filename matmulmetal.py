b7b34243218ed41636a8963fccbbc41747b9a0ef
Rory Clear <roryclear.rc@gmail.com>
2023-03-20 19:06:46 +0000

    remove some of the mess

--- matmulmetal.py
+++ matmulmetal.py
@@ -26,28 +26,12 @@ options = Metal.MTLCompileOptions.alloc().init()
 library = unwrap(device.newLibraryWithSource_options_error_(prg, options, None))
 fxn = library.newFunctionWithName_("matmul")
 pipeline_state = unwrap(device.newComputePipelineStateWithFunction_error_(fxn, None))
-print("HERE")
 
 
 encoder.setComputePipelineState_(pipeline_state)
-print("HERE")
-#a = pipeline_state.maxTotalThreadsPerThreadgroup()
-#print(a)
 
 a = np.random.rand(128).astype(np.float32)
-a[0] = np.float32(420)
 a_buffer = device.newBufferWithLength_options_(a.nbytes ,1)
-
-y = a_buffer.contents()
-print(y.__getitem__(0),y.__getitem__(1),y.__getitem__(2),y.__getitem__(3))
-
-print("x42??")
-a_buffer.contents().__setitem__(0,b'\x42')
-
-y = a_buffer.contents()
-print(y.__getitem__(0),y.__getitem__(1),y.__getitem__(2),y.__getitem__(3))
-
-#a_buffer.contents()
 
 encoder.setBuffer_offset_atIndex_(a_buffer, 0, 0)
 
@@ -56,33 +40,14 @@ encoder.endEncoding()
 command_buffer.commit()
 command_buffer.waitUntilCompleted()
 
-s = ""
-n = 0
 
-x = a_buffer.contents
 y = a_buffer.contents()
-print(y)
-print("after function")
-print(y.__getitem__(0),y.__getitem__(1),y.__getitem__(2),y.__getitem__(3))
-y.__setitem__(0,b'\x69')
-print(y.__getitem__(0),y.__getitem__(1),y.__getitem__(2),y.__getitem__(3))
 
-s = y.__getitem__(0) + y.__getitem__(1) + y.__getitem__(2) + y.__getitem__(3)
-print("s =",s)
-n = struct.unpack('1f', s)
+s = y.__getitem__(0)
+for i in range(1,512):
+    s += y.__getitem__(i)
+output = struct.unpack('128f',s)
+print(output)
 
-print("n =",n)
 
-#a_buffer.contents().__setitem__(0,1)
-
-#print(a_buffer.contents())
-#for i in a_buffer.contents():
-#    if "x00" not in str(i):
-#        print(str(i)[2:len(str(i))-1])
-#        s += str(i)[2:len(str(i))-1]
-#    n+=1
-#    #print(n)
-#print(s)
-#print(type(defaultLibrary))
-
-print("hello!")
+print("done")
