ae8826698e3db61b9b6d343db45478b961ea43d7
Rory Clear <roryclear.rc@gmail.com>
2023-03-26 17:17:36 +0100

    metal can do different shapes now

--- matmulmetal.py
+++ matmulmetal.py
@@ -5,6 +5,10 @@ import numpy as np
 import struct
 
 def matmul(a,b,length):
+    aRows = a.shape[0]
+    aCols = a.shape[1]
+    bRows = b.shape[0]
+    bCols = b.shape[1]
     device = Metal.MTLCreateSystemDefaultDevice()
     mtl_queue = device.newCommandQueue()
     command_buffer = mtl_queue.commandBuffer()
@@ -14,20 +18,22 @@ def matmul(a,b,length):
     using namespace metal;
     kernel void matmul(device float *a,
                         device float *b,
-                        device int& length,
+                        device int& aRows,
+                        device int& aCols,
+                        device int& bCols,
                         device float *res,
     uint index [[thread_position_in_grid]])
     {{     
-          int row = index / length;
-          int col = index % length;
+          int row = index / bCols;
+          int col = index % bCols;
           float total = 0;
-          if(row < length && col < length) 
+          if(row < aRows && col < bCols) 
           {{
-            for(int i = 0; i < length; i++)
+            for(int i = 0; i < aCols; i++)
             {{
-              total += a[row * length + i] * b[col + i * length];
+              total += a[row * aCols + i] * b[col + i * bCols];
             }}
-            res[row * length + col] = total;
+            res[row * bCols + col] = total;
           }}
     }}"""
 
@@ -45,22 +51,30 @@ def matmul(a,b,length):
     m = b_buffer.contents().as_buffer(b.nbytes)
     m[:] = bytes(b)
 
-    n = np.int32(length)
-    n_buffer = device.newBufferWithLength_options_(4 ,1)
+    aRows = np.int32(aRows)
+    aCols = np.int32(aCols)
+    bCols = np.int32(bCols)
+    aRows_buffer = device.newBufferWithLength_options_(4 ,1)
+    aCols_buffer = device.newBufferWithLength_options_(4 ,1)
+    bCols_buffer = device.newBufferWithLength_options_(4 ,1)
     for i in range(4):
-        n_buffer.contents().__setitem__(i,n.tobytes()[i].to_bytes(1,'big'))
+        aRows_buffer.contents().__setitem__(i,aRows.tobytes()[i].to_bytes(1,'big'))
+        aCols_buffer.contents().__setitem__(i,aCols.tobytes()[i].to_bytes(1,'big'))
+        bCols_buffer.contents().__setitem__(i,bCols.tobytes()[i].to_bytes(1,'big'))
 
-    res = np.empty_like(b)
+    res = np.empty([aRows, bCols]).astype(np.float32).flatten()
     res_buffer = device.newBufferWithLength_options_(res.nbytes ,1)
 
     encoder.setBuffer_offset_atIndex_(a_buffer, 0, 0)
     encoder.setBuffer_offset_atIndex_(b_buffer, 0, 1)
-    encoder.setBuffer_offset_atIndex_(n_buffer, 0, 2)
-    encoder.setBuffer_offset_atIndex_(res_buffer, 0, 3)
+    encoder.setBuffer_offset_atIndex_(aRows_buffer, 0, 2)
+    encoder.setBuffer_offset_atIndex_(aCols_buffer, 0, 3)
+    encoder.setBuffer_offset_atIndex_(bCols_buffer, 0, 4)
+    encoder.setBuffer_offset_atIndex_(res_buffer, 0, 5)
     threadGroupSize = pipeline_state.maxTotalThreadsPerThreadgroup()
     if length < threadGroupSize:
         threadGroupSize = length
-    encoder.dispatchThreads_threadsPerThreadgroup_(Metal.MTLSizeMake(length*length,1,1), Metal.MTLSizeMake(threadGroupSize,1,1))
+    encoder.dispatchThreads_threadsPerThreadgroup_(Metal.MTLSizeMake(aRows*bCols,1,1), Metal.MTLSizeMake(threadGroupSize,1,1))
     encoder.endEncoding()
     command_buffer.commit()
     command_buffer.waitUntilCompleted()
--- metalTest.py
+++ metalTest.py
@@ -3,8 +3,8 @@ import matmulmetal as mmm
 
 length = 128
 
-a = np.random.rand(length,length).astype(np.float32)
-b = np.random.rand(length,length).astype(np.float32)
+a = np.random.rand(200,length).astype(np.float32)
+b = np.random.rand(length,190).astype(np.float32)
 answer = np.matmul(a,b).flatten()
 output = mmm.matmul(a,b,length)
 assert np.allclose(output, answer)
