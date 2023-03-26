64af9e1be611059514e97c50adec3697b3e1b95e
Rory Clear <roryclear.rc@gmail.com>
2023-03-26 18:52:01 +0100

    in line with ipynb file

--- cudaTest.py
+++ cudaTest.py
@@ -3,9 +3,10 @@ import matmulcuda as mmc
 
 length = 128
 
-a = np.random.rand(length,length).astype(np.float32)
-b = np.random.rand(length,length).astype(np.float32)
+a = np.random.rand(200,length).astype(np.float32)
+b = np.random.rand(length,190).astype(np.float32)
 answer = np.matmul(a,b).flatten()
-output = mmc.matmul(a,b,length)
+output = mmc.matmul(a,b)
 assert np.allclose(output, answer)
 print("passed")
+     
--- matmulcuda.py
+++ matmulcuda.py
@@ -2,33 +2,35 @@ import pycuda.compiler as comp
 import pycuda.driver as cuda
 import pycuda.autoinit
 import numpy as np
+import math
 
-def matmul(a,b,length):
-    a = a.flatten()
-    b = b.flatten()
+def matmul(a,b):
+    aRows = a.shape[0]
+    aCols = a.shape[1]
+    bCols = b.shape[1]
     a_g = cuda.mem_alloc(a.nbytes)
     cuda.memcpy_htod(a_g,a)
     b_g = cuda.mem_alloc(b.nbytes)
     cuda.memcpy_htod(b_g,b)
-    res_g = cuda.mem_alloc(a.nbytes)
-    res_np = np.empty_like(a)#fix later
+    res_np = np.empty([aRows, bCols]).astype(np.float32).flatten()
+    res_g = cuda.mem_alloc(aRows * bCols * 4)
     cuda.memcpy_htod(res_g,res_np)
 
     mod = comp.SourceModule(
       """
-    __global__ void matmul(float *a, float *b, int length, float *res)
+    __global__ void matmul(float *a, float *b, int aRows, int aCols, int bCols, float *res)
   {
     const int gid = threadIdx.x + (blockDim.x * blockIdx.x);
-    int row = gid / length;
-    int col = gid % length;
+    int row = gid / bCols;
+    int col = gid % bCols;
     float total = 0;
-    if(row < length && col < length) 
+    if(row < aRows && col < bCols) 
     {
-      for(int i = 0; i < length; i++)
+      for(int i = 0; i < aCols; i++)
       {
-        total += a[row * length + i] * b[col + i * length];
+        total += a[row * aCols + i] * b[col + i * bCols];
       }
-      res[row * length + col] = total;
+      res[row * bCols + col] = total;
     }
   }
   """
@@ -36,6 +38,9 @@ def matmul(a,b,length):
     matmulcuda = mod.get_function("matmul")
     numberOfThreads = \
         cuda.Device(0).get_attribute(pycuda._driver.device_attribute.MAX_THREADS_PER_BLOCK)
-    matmulcuda(a_g,b_g,np.int32(length),res_g,block=(1024,1,1),grid=(16,1)) #todo fix these sizes
+    ops = aRows * bCols
+    bx = min(ops,numberOfThreads)
+    gx = math.ceil(ops / numberOfThreads)
+    matmulcuda(a_g,b_g,np.int32(aRows),np.int32(aCols),np.int32(bCols),res_g,block=(bx,1,1),grid=(gx,1))
     cuda.memcpy_dtoh(res_np,res_g)
     return res_np
