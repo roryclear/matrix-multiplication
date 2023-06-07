0781cb383fd5b93047bdaca1a8513d1cb252981f
Rory Clear <roryclear.rc@gmail.com>
2023-06-07 17:32:01 +0100

    tile attempt

--- matmul.cpp
+++ matmul.cpp
@@ -27,12 +27,35 @@ int inners = dim;
             left[row * columns + inner] * right[inner * columns + col];
 } } } }
 
+inline void matmulTiling(const float *left, const float *right,
+                            float *result, int dim, int tileSize) {
+int rows = dim;
+int columns = dim;
+int inners = dim;
+
+  for(int rowTile = 0; rowTile < rows; rowTile+=tileSize) {
+    for (int row = rowTile; row < rowTile+tileSize; row++) {
+      for(int innerTile = 0; innerTile < inners; innerTile+=tileSize) {
+        for (int inner = innerTile; inner < innerTile+tileSize; inner++) {
+          for(int colTile = 0; colTile < columns; colTile+=tileSize) {
+            for (int col = colTile; col < colTile+tileSize; col++) {
+              result[row * columns + col] +=
+                  left[row * columns + inner] * right[inner * columns + col];
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
 int main() {
     const int dim = 512;
     float left[dim*dim] = {};
     float right[dim*dim] = {};
     float resultA[dim*dim] = {};
     float resultB[dim*dim] = {};
+    float resultC[dim*dim] = {};
     for(int i = 0; i < dim*dim; i++) {
         left[i] = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
         right[i] = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
@@ -43,13 +66,36 @@ int main() {
 
     tStart = clock();
     matmulFaster(left,right,resultB,dim);
-    printf("Time taken: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+    printf("Time taken (reorder): %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+
+    int tileSize = 1;
+    while(tileSize < dim) {
+      float resultC[dim*dim] = {};
+      tStart = clock();
+      matmulTiling(left,right,resultC,dim,tileSize);
+      printf("Time taken (reorder + tiling): %.2fs tileSize = %d \n", (double)(clock() - tStart)/CLOCKS_PER_SEC, tileSize);
+      for(int i = 0; i < dim*dim; i++) {
+        if(resultA[i] != resultC[i]) {
+            return 0;
+        }
+      }
+      tileSize += tileSize;
+    }
 
     for(int i = 0; i < dim*dim; i++) {
         if(resultA[i] != resultB[i]) {
             return 0;
         }
     }
-    std::cout << "outputs equal";
+    printf("reorder outputs equal\n");
+
+    for(int i = 0; i < dim*dim; i++) {
+        if(resultA[i] != resultC[i]) {
+            return 0;
+        }
+    }
+
+    printf("tiling outputs equal");
+
     return 0;
 }
