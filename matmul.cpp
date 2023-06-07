37f0ed4cff3135c458762e00b4cff7a6df1addc7
unknown <roryclear.rc@gmail.com>
2023-06-07 20:52:03 +0100

    actual working tiling

--- matmul.cpp
+++ matmul.cpp
@@ -2,6 +2,8 @@
 #include <random>
 #include <time.h>
 
+//g++ -O3 -march=native -ffast-math matmul.cpp -o a
+
 inline void matmulImplNaive(const float *left, const float *right,
                             float *result, int dim) {
 int rows = dim;
@@ -17,23 +19,21 @@ int inners = dim;
 
 inline void matmulFaster(const float *left, const float *right,
                             float *result, int dim) {
-int rows = dim;
-int columns = dim;
-int inners = dim;
-  for (int row = 0; row < rows; row++) {
-    for (int inner = 0; inner < inners; inner++) {
-      for (int col = 0; col < columns; col++) {
-        result[row * columns + col] +=
-            left[row * columns + inner] * right[inner * columns + col];
+  for (int row = 0; row < dim; row++) {
+    for (int inner = 0; inner < dim; inner++) {
+      for (int col = 0; col < dim; col++) {
+        result[row * dim + col] +=
+            left[row * dim + inner] * right[inner * dim + col];
 } } } }
+
 
 inline void matmulTiling(const float *left, const float *right,
                             float *result, int dim, int tileSize) {
   for(int rowTile = 0; rowTile < dim; rowTile+=tileSize) {
-    for (int row = rowTile; row < rowTile+tileSize; row++) {
-      for(int innerTile = 0; innerTile < dim; innerTile+=tileSize) {
-        for (int inner = innerTile; inner < innerTile+tileSize; inner++) {
-          for(int colTile = 0; colTile < dim; colTile+=tileSize) {
+    for (int innerTile = 0; innerTile < dim; innerTile+=tileSize) {
+      for(int colTile = 0; colTile < dim; colTile+=tileSize) {
+        for (int row = rowTile; row < rowTile+tileSize; row++) {
+          for(int inner = innerTile; inner < innerTile+tileSize; inner++) {
             for (int col = colTile; col < colTile+tileSize; col++) {
               result[row * dim + col] +=
                   left[row * dim + inner] * right[inner * dim + col];
@@ -45,22 +45,8 @@ inline void matmulTiling(const float *left, const float *right,
   }
 }
 
-inline void matmulTiling2(const float *left, const float *right,
-                            float *result, int dim, int tileSize) {
-    for (int row = 0; row < dim; row++) {
-      for(int innerTile = 0; innerTile < dim; innerTile+=tileSize) {
-        for (int inner = innerTile; inner < innerTile+tileSize; inner++) {
-            for (int col = 0; col < dim; col++) {
-              result[row * dim + col] +=
-                  left[row * dim + inner] * right[inner * dim + col];
-            }
-        }
-      }
-    }
-}
-
 int main() {
-    const int dim = 1024;
+    const int dim = 2048;
     float *left =  new float[dim*dim];
     float *right =  new float[dim*dim];
     float *resultA =  new float[dim*dim];
@@ -78,7 +64,7 @@ int main() {
     matmulFaster(left,right,resultB,dim);
     printf("Time taken (reorder): %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
 
-    int tileSize = 1;
+    int tileSize = 2;
     while(tileSize < dim) {
       resultC =  new float[dim*dim];
       tStart = clock();
