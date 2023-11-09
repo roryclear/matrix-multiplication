a779e886b68fe9d423e5f400bdc494887531bdcd
Rory Clear <roryclear.rc@gmail.com>
2023-11-09 15:15:07 +0000

    get best of 20 times:

--- metaltest2.py
+++ metaltest2.py
@@ -3,7 +3,7 @@ import matmulmetal2 as mmm
 import matmulopencl as mmo
 import time
 import torch
-import tensorflow as tf
+#import tensorflow as tf
 
 length = 2048
 
@@ -14,24 +14,34 @@ answer = np.empty_like(a)
 
 b2 = np.zeros_like(b)
 
-start_time = time.time()
-om = mmm.matmul(a,b)
-metal_time = time.time() - start_time
+metal_time = None
+for _ in range(20):
+	start_time = time.time()
+	om = mmm.matmul(a,b)
+	t = time.time() - start_time
+	if metal_time == None or t < metal_time:
+		metal_time = t
 print("--- metal\t%.5f seconds ---" % (metal_time))
 
-start_time = time.time()
-answer = np.matmul(a,b).flatten()
-print("--- numpy\t%.5f seconds ---" % (time.time() - start_time))
+fastest = None
+for _ in range(20):
+	start_time = time.time()
+	answer = np.matmul(a,b).flatten()
+	t = time.time() - start_time
+	if fastest == None or t < fastest:
+		fastest = t
+print("--- numpy\t%.5f seconds ---" % (fastest))
 
 at = torch.from_numpy(a)
 bt = torch.from_numpy(b)
-start_time = time.time()
-t = torch.matmul(at,bt).flatten()
-print("--- torch\t%.5f seconds ---" % (time.time() - start_time))
-
-start_time = time.time()
-c = tf.matmul(a, b)
-print("--- tensorflow\t%.5f seconds ---" % (time.time() - start_time))
+fastest = None
+for _ in range(20):
+	start_time = time.time()
+	x = torch.matmul(at,bt).flatten()
+	t = time.time() - start_time
+	if fastest == None or t < fastest:
+		fastest = t
+print("--- torch\t%.5f seconds ---" % (fastest))
 
 assert np.allclose(om, answer)
 print("passed")
