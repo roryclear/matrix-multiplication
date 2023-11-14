82c2024ac0027ba26696fb60e18495bb91562b5f
Rory Clear <roryclear.rc@gmail.com>
2023-11-14 11:33:06 +0000

    start sum too

---
+++ sum_metal.py
@@ -0,0 +1,47 @@
+import objc
+import Metal
+import Foundation
+import numpy as np
+import struct
+
+device = Metal.MTLCreateSystemDefaultDevice()
+
+def sum(a):
+    dim = len(a)
+    mtl_queue = device.newCommandQueue()
+    command_buffer = mtl_queue.commandBuffer()
+    encoder = command_buffer.computeCommandEncoder()
+
+    prg = f"""#include <metal_stdlib>
+    #include <metal_simdgroup_matrix>
+    using namespace metal;
+    kernel void sum(device float *res,
+                        device const float *a,
+                        uint3 gid [[threadgroup_position_in_grid]], uint3 lid [[thread_position_in_threadgroup]])
+    {{   
+    }}"""
+
+    options = Metal.MTLCompileOptions.alloc().init()
+    library, err = device.newLibraryWithSource_options_error_(prg, options, None)
+    fxn = library.newFunctionWithName_("sum")
+    pipeline_state, err = device.newComputePipelineStateWithFunction_error_(fxn, None)
+    encoder.setComputePipelineState_(pipeline_state)
+
+    size = dim*dim*4
+    a_buffer = device.newBufferWithLength_options_(size ,1)
+    m = a_buffer.contents().as_buffer(size)
+    m[:] = bytes(a)
+
+    res_buffer = device.newBufferWithLength_options_(size ,1)
+
+    encoder.setBuffer_offset_atIndex_(res_buffer, 0, 0)
+    encoder.setBuffer_offset_atIndex_(a_buffer, 0, 1)
+    threadsPerGrid = Metal.MTLSizeMake(1,1,1)
+    threadsPerThreadGroup = Metal.MTLSizeMake(1,1,1)
+    encoder.dispatchThreadgroups_threadsPerThreadgroup_(threadsPerGrid, threadsPerThreadGroup)
+    encoder.endEncoding()
+    command_buffer.commit()
+    command_buffer.waitUntilCompleted()
+    output = np.asarray(res_buffer.contents().as_buffer(size))
+    output = np.frombuffer(output, dtype=np.float32)
+    return output
---
+++ test_metal_sum.py
@@ -0,0 +1,33 @@
+import numpy as np
+import sum_metal as sm
+import matmulopencl as mmo
+import time
+import torch
+#import tensorflow as tf
+
+length = 2048
+
+a = np.ones((length, length),dtype=np.float32)
+fastest = None
+answer = None
+for i in range(20):
+	start_time = time.time()
+	answer = np.sum(a)
+	t = time.time() - start_time
+	if fastest == None or t < start_time:
+		fastest = t
+print(answer)
+print("--- numpy\t%.5f seconds ---" % (fastest))
+
+metal_time = None
+for _ in range(20):
+	start_time = time.time()
+	om = sm.sum(a)
+	t = time.time() - start_time
+	if metal_time == None or t < metal_time:
+		metal_time = t
+print("--- metal\t%.5f seconds ---" % (metal_time))
+
+
+assert np.allclose(om, answer)
+print("passed")
