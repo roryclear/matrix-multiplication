24f0936f83a34b1d44d10fb958f57939e17808ce
unknown <roryclear.rc@gmail.com>
2023-12-09 19:50:23 +0200

    wider block more speed

--- matmulopencl.py
+++ matmulopencl.py
@@ -5,7 +5,7 @@ def matmul(a,b):
 	dim = len(a)
 	#print("dim =",dim)
 	ys = 2;
-	xs = 2;
+	xs = 4;
 
 	a = a.flatten()
 	b = b.flatten()
@@ -31,21 +31,33 @@ def matmul(a,b):
 	int endY = startY + ({dim} / {local_0});
 	for(int y = startY; y < endY; y+={ys}) {{
 		for(int x = 0; x < {dim}; x+={xs}) {{
-			float acc[2][2];
+			float acc[2][4];
 			acc[0][0] = 0;
 			acc[0][1] = 0;
+			acc[0][2] = 0;
+			acc[0][3] = 0;
 			acc[1][0] = 0;
 			acc[1][1] = 0;
+			acc[1][2] = 0;
+			acc[1][3] = 0;
 			for(int k = 0; k < {dim}; k++) {{
 				acc[0][0] += a[y * {dim} + k] * b[x * {dim} + k];
 				acc[0][1] += a[y * {dim} + k] * b[(x + 1) * {dim} + k];
+				acc[0][2] += a[y * {dim} + k] * b[(x + 2) * {dim} + k];
+				acc[0][3] += a[y * {dim} + k] * b[(x + 3) * {dim} + k];
 				acc[1][0] += a[(y + 1) * {dim} + k] * b[(x) * {dim} + k];
 				acc[1][1] += a[(y + 1) * {dim} + k] * b[(x + 1) * {dim} + k];
+				acc[1][2] += a[(y + 1) * {dim} + k] * b[(x + 2) * {dim} + k];
+				acc[1][3] += a[(y + 1) * {dim} + k] * b[(x + 3) * {dim} + k];
 			}}
 			res[y * {dim} + x] = acc[0][0];
 			res[y * {dim} + x + 1] = acc[0][1];
+			res[y * {dim} + x + 2] = acc[0][2];
+			res[y * {dim} + x + 3] = acc[0][3];
 			res[(y + 1) * {dim} + x] = acc[1][0];
 			res[(y + 1) * {dim} + x + 1] = acc[1][1];
+			res[(y + 1) * {dim} + x + 2] = acc[1][2];
+			res[(y + 1) * {dim} + x + 3] = acc[1][3];
 		}}
 	}}
 	}}
