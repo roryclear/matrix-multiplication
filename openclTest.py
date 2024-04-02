f38eac8ae3eb66f9ec3ff65adc4612d1eb006ba5
unknown <roryclear.rc@gmail.com>
2024-04-02 19:28:56 +0800

    use perf counter for time

--- openclTest.py
+++ openclTest.py
@@ -15,9 +15,9 @@ answer = np.empty_like(a)
 
 opencl_time = None
 for _ in range(20):
-	start_time = time.time()
+	start_time = time.perf_counter()
 	oo = mmo.matmul(a,b)
-	t = time.time() - start_time
+	t = time.perf_counter() - start_time
 	if opencl_time == None or t < opencl_time:
 		opencl_time = t
 		
@@ -25,9 +25,9 @@ print("--- opencl\t%.5f seconds ---" % (opencl_time))
 
 fastest = None
 for _ in range(20):
-	start_time = time.time()
+	start_time = time.perf_counter()
 	answer = np.matmul(a,b).flatten()
-	t = time.time() - start_time
+	t = time.perf_counter() - start_time
 	if fastest == None or t < fastest:
 		fastest = t
 print("--- numpy\t%.5f seconds ---" % (fastest))
@@ -37,9 +37,9 @@ at = torch.from_numpy(a)
 bt = torch.from_numpy(b)
 fastest = None
 for _ in range(20):
-	start_time = time.time()
+	start_time = time.perf_counter()
 	x = torch.matmul(at,bt).flatten()
-	t = time.time() - start_time
+	t = time.perf_counter() - start_time
 	if fastest == None or t < fastest:
 		fastest = t
 print("--- torch\t%.5f seconds ---" % (fastest))
